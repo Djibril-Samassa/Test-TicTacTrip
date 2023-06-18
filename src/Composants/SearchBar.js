81cce0e5ac63a6131dbfc55f55b62c2dcb124569
Djibril <djibrilsamassa.pro@gmail.com>
2023-06-18 20:36:36 +0200

    City fetching commit

--- src/Composants/SearchBar.js
+++ src/Composants/SearchBar.js
@@ -6,15 +6,22 @@ import Select from "react-select";
 import { Icon } from "@iconify/react";
 
 export default function SearchBar() {
-  const [accommodation, setAccommodation] = useState(false);
-  const [travelOption, setTravelOption] = useState({});
-
-  // style à appliquer sur les input de texte (S    pécifique à React Select)
+  // style à appliquer sur les input de texte (Spécifique à React Select)
   const TextInputStyle = {
     control: (base) => ({
       ...base,
       border: 0,
       boxShadow: "none",
+    }),
+  };
+
+  const TextInputStyle2 = {
+    control: (base) => ({
+      ...base,
+      border: "none",
+      boxShadow: "none",
+      backgroundColor: "#F1F2F6",
+      color: "#a0a8c2",
     }),
   };
 
@@ -25,19 +32,76 @@ export default function SearchBar() {
     ],
   };
 
-  // Les villes les plus populaires
-  const [popularCities, setPopularCities] = useState([]);
+  const [accommodation, setAccommodation] = useState(false);
+  const [travelOption, setTravelOption] = useState({});
+
+  // => On enregistre la ville de départ et d'arrivée
+  // => L'objet entier on ne sait jamais s'il y a d'autres champs dont on aura besoin
+  const [choosenCities, setChoosenCities] = useState({
+    start: {},
+    finish: {},
+  });
+
+  // La liste affiché dans la Select List => Soit les villes populaires, soit la liste des villes par lettre
+  const [startCities, setStartCities] = useState([]);
+  const [finishCities, setFinishCities] = useState([]);
 
   useEffect(() => {
-    GetPopularCities()
+    GetPopularCities("start");
   }, []);
 
   // Récuperer les villes les plus populaires
-  const GetPopularCities = () => {
-    axios.get("https://api.comparatrip.eu/cities/popular/5").then((res) => {
-      setPopularCities(res.data);
-    });
+  // De départ ou d'arrivée avec une condition qui vérifie
+  const GetPopularCities = (event) => {
+    if (event === "start") {
+      axios.get("https://api.comparatrip.eu/cities/popular/5").then((res) => {
+        // Ce sera les propositions par défaut
+        setStartCities(res.data);
+      });
+    } else if (event === "finish") {
+      axios
+        .get(
+          `https://api.comparatrip.eu/cities/popular/from/${choosenCities.start.unique_name}/5`
+        )
+        .then((res) => {
+          // Ce sera les propositions par défaut
+          setFinishCities(res.data);
+        });
+    } else {
+      alert("Un problème est survenu");
+    }
   };
+
+  // Recherchez ville par le nom
+  const SearchCityByName = (e, event) => {
+    const name = e.target.value;
+    if (event === "start") {
+      axios
+        .get(`https://api.comparatrip.eu/cities/autocomplete/?q=${name}`)
+        .then((res) => {
+          // On propose les villes de cette liste sur la SelectList
+          setStartCities(res.data);
+        });
+    } else if (event === "finish") {
+      axios
+        .get(`https://api.comparatrip.eu/cities/autocomplete/?q=${name}`)
+        .then((res) => {
+          // On propose les villes de cette liste sur la SelectList
+          setFinishCities(res.data);
+        });
+    } else {
+      alert("Une erreur est survenue");
+    }
+  };
+
+  // Quand une ville de départ est sélectionnée, on récupère la liste des villes d'arrivée les plus populaires depuis ces villes
+  useEffect(() => {
+    console.log(choosenCities.start);
+    if (choosenCities.start.unique_name) {
+      GetPopularCities("finish");
+    } else {
+    }
+  }, [choosenCities]);
 
   return (
     <div>
@@ -59,27 +123,41 @@ export default function SearchBar() {
           styles={TextInputStyle}
         /> */}
       </div>
+
+      
       {/* Informations sur les dates et destinations du trajet */}
       <div>
-        {/* Input avec une icone */}
-        <div className={Style.inputWithIcon}>
-          <Icon icon="game-icons:position-marker" className={Style.inputIcon} />
-          <input
-            type="text"
-            className={Style.inputField}
-            placeholder="From: City, Station or Airport"
-          />
-        </div>
+        <Select
+          onKeyDown={(e) => {
+            SearchCityByName(e, "start");
+          }}
+          type="text"
+          className={Style.inputField}
+          placeholder="From: City, Station or Airport"
+          styles={TextInputStyle2}
+          options={startCities}
+          // Renseigner ce qui sera affiché dans la select liste si le nom de clé n'est pas "label"
+          getOptionLabel={(option) => option.local_name}
+          // Renseigner ce qui sera affiché dans la select liste si le nom de clé n'est pas "value"
+          getOptionValue={(option) => option.local_name}
+          // On enregistre la ville sélectionné dans la variable choosenCities
+          onChange={async (option) => {
+            // Quand une ville de départ est selectionnée on l'enregistre
+            await setChoosenCities({ ...choosenCities, start: option });
+          }}
+        />
 
-        {/* Input avec une icone */}
-        <div className={Style.inputWithIcon}>
-          <Icon icon="game-icons:position-marker" className={Style.inputIcon} />
-          <input
-            type="text"
-            className={Style.inputField}
-            placeholder="To: City, Station or Airport"
-          />
-        </div>
+        <Select
+          type="text"
+          className={Style.inputField}
+          placeholder="To: City, Station or Airport"
+          styles={TextInputStyle2}
+          options={finishCities}
+          // Renseigner ce qui sera affiché dans la select liste si le nom de clé n'est pas "label"
+          getOptionLabel={(option) => option.local_name}
+          // Renseigner ce qui sera affiché dans la select liste si le nom de clé n'est pas "value"
+          getOptionValue={(option) => option.local_name}
+        />
       </div>
       {/* Activer affichage d'hébergement */}
       <div></div>
